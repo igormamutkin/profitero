3cbcf7311135339193d96bb97207534efd29e471
Igor <igorek1mamutkin@gmail.com>
2020-10-22 20:30:30 +0300

    final123

--- day2/task05.rb
+++ day2/task05.rb
@@ -1,19 +1,24 @@
+puts "Day"
 day = gets.chomp.to_i
+puts "Month"
 month = gets.chomp.to_i
+Puts "Year"
 year = gets.chomp.to_i
 
-months = { jan: 31, 
-		 feb: 28, 
-		 mar: 31, 
-		 apr: 30, 
-		 may: 31, 
-		 jun: 30, 
-		 jul: 31, 
-		 aug: 31, 
-		 sep: 30, 
-		 oct: 31, 
-		 nov: 30, 
-		 dec: 31}
+months = { 
+	jan: 31, 
+	feb: 28, 
+	mar: 31, 
+	apr: 30, 
+	may: 31, 
+	jun: 30, 
+	jul: 31, 
+	aug: 31, 
+	sep: 30, 
+	oct: 31, 
+	nov: 30, 
+	dec: 31}
+
 flag = 1;
 result = 0;
 
